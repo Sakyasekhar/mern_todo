fcb21fd1357b1ebd496347ce7c948edca77ddd97
sakya_sekhar <sakyasekhar.g@gmail.com>
2023-06-01 23:38:01 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -1,34 +1,46 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
-const Routes = require('./routes/todo_routes');
+const bodyParser = require('body-parser');
+
+
+const Blog = require('./models/blog'); 
 
 const mongoose = require('mongoose');
 
-// const dbURI = "mongodb+srv://Sakyasekhar:sakyasekhar@chatapp.mlveze6.mongodb.net/?retryWrites=true&w=majority"; 
+app.use(bodyParser.json());
+
+const dbURI = "mongodb+srv://Sakyasekhar:sakyasekhar@chatapp.mlveze6.mongodb.net/?retryWrites=true&w=majority"; 
  
-const dbURI = "mongodb://localhost:27017";
+// const dbURI = "mongodb://localhost:27017";
 mongoose.connect(dbURI,{ useNewUrlParser: true, useUnifiedTopology: true })
-.then((result) => app.listen(3000))
-.catch((err) => console.log(err))
+.then(
+  result => {console.log('server connected with db')}
+)
+.catch(err => console.log(err));
 
 
-app.set('view engine','ejs');
 
-//middleware and static files
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
-app.use((req, res, next) => {
-  res.locals.path = req.path;
-  next();
+
+
+
+// app.use('/', Routes);
+app.post('/create',async(req,res)=>{
+
+  console.log("hii");
+  console.log(req.body)
+  const {title,author } = req.body;
+  let newUser = new Blog({
+    title,author
+  })
+  
+  await newUser.save();
+  
 });
 
-// task routes
-app.use('/', Routes);
+app.listen(8080, (err) => {
+  if (err) throw err;
+  console.log('okk')
+});
 
 
 
-// app.use((req, res) => {  //fires for every single request|| functions invocs only if express dont find any url matchs above
-//   res.status(404).render('404', { title: '404' });
-// });
